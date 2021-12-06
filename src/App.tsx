4c644b8651cdca8e395ddb5fa8ab596ee7ced743
shivkumarprajapat <shivkumarprajapat28@gmail.com>
2021-12-06 20:27:51 +0530

    typescript fundamentals

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function sum(a: number, b: number) {
+  return a + b
+}
+interface Props {
+  a: number;
+  b: number
+}
+
+function SumComonent(props: Props) {
+  return <>{props.a + props.b}</>
+}
 function App() {
+
+  const value = sum(5, 6)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Sum of to number {value}</h1>
+        <h1>Sum of to number <SumComonent a={5} b={6} /></h1>
       </header>
     </div>
   );
